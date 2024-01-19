4287d2436d98dc9c20c0dd5ccf9527e103086a35
jf201 <jf@2null1.com>
2024-01-19 19:23:00 +0100

    python lsp added

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(doom-themes-neotree-file-icons t)
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(package-selected-packages '(lsp-ui php-mode lsp-mode)))
+ '(package-selected-packages '(lsp-python-ms lsp-pyright lsp-ui php-mode lsp-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
